5e7bb08aff29a80416a286f31d01138c1deeaee8
Progga Roy~ <proggaroy2000@gmail.com>
2023-03-15 04:11:26 -0700

    modal add and card show

--- JS/meals.js
+++ JS/meals.js
@@ -1,13 +1,15 @@
 
-const loadMeal = () => {
-  fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Fish')
+const loadMeal = (searchText) => {
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+  fetch(url)
     .then(res => res.json())
     .then(data => displayMeal(data.meals))
 }
- 
+
 const displayMeal = (meals) => {
   console.log(meals)
   const mealContainer = document.getElementById('meal-container');
+  mealContainer.innerText = '';
   meals.forEach(meal => {
     const createDiv = document.createElement('div')
     createDiv.classList.add('col')
@@ -16,11 +18,35 @@ const displayMeal = (meals) => {
         <img src="${meal.strMealThumb}" class="card-img-top" alt="${meal.strMeal}">
         <div class="card-body">
           <h5 class="card-title">${meal.strMeal}</h5>
-          <p class="card-text">${meal.strInstructions}</p>
+          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus ab error quasi quidem ea velit sunt reprehenderit optio voluptatum sed?</p>
+          <button onclick="loadMealDetail(${meal.idMeal})" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealDB">
+             Details
+          </button>
         </div>
       </div>`
     mealContainer.appendChild(createDiv);
   });
 }
 
-loadMeal();
+const searchMeal = () => {
+  const searchField = document.getElementById('search-field').value;
+  loadMeal(searchField);
+}
+
+const loadMealDetail =idMeal=>{
+const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+fetch(url)
+.then(res=>res.json())
+.then(data=> displayMealDetail(data.meals[0]))
+
+}
+
+
+const displayMealDetail =meal=>{
+document.getElementById('mealDBLabel').innerText = meal.strMeal;
+const img = document.getElementById('mealDetailBody');
+img.innerHTML =`
+<img class="img-fluid" src="${meal.strMealThumb}" alt="" srcset="">
+`
+}
+loadMeal('fish');
